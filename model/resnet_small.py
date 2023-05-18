4e40c43fade7b738a936c368c49a8fd081878dc8
Jingwen Wu <67344043+jingwenw15@users.noreply.github.com>
2023-05-17 19:27:15 -0700

    also take out layer 2 from resnet small

--- model/resnet_small.py
+++ model/resnet_small.py
@@ -38,6 +38,8 @@ class Net(nn.Module):
 
         self.resnet = models.resnet18(weights='IMAGENET1K_V1') # TODO: later on, don't use pretrained weights
         in_features = self.resnet.fc.in_features
+        # TODO: also take out layer 2 to see 
+        self.resnet.layer2 = nn.Sequential(nn.Identity()) # take out layer 2
         self.resnet.layer3 = nn.Sequential(nn.Identity()) # take out layer 3 
         self.resnet.layer4 = nn.Sequential(nn.Identity()) # take out layer 4 
         
@@ -46,7 +48,7 @@ class Net(nn.Module):
         #     param.requires_grad = False 
 
         # replace FC layer with our layer 
-        self.resnet.fc = nn.Linear(in_features=128, out_features=4, device=device)
+        self.resnet.fc = nn.Linear(in_features=64, out_features=4, device=device)
         print(self.resnet)
         self.resnet = self.resnet.to(device)
 
