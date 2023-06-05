ade8fc44a25c144e9890b1cd3ecf493be9291505
Jingwen Wu <67344043+jingwenw15@users.noreply.github.com>
2023-06-04 20:27:58 -0700

    mse loss -> change to float

--- model/vgg.py
+++ model/vgg.py
@@ -69,7 +69,7 @@ def ce_loss(outputs, labels):
     return F.cross_entropy(outputs, labels, reduction='mean')
 
 def mse_loss(outputs, labels): 
-    outputs_preds = torch.argmax(outputs, dim=1) 
+    outputs_preds = torch.argmax(outputs, dim=1).float()
     return F.mse_loss(outputs_preds, labels, reduction='mean')
 
 
